ab6e6921bf05f8faa84e6bfd51fa0159f9afee9e
Stavros <steveiliop56@gmail.com>
2024-11-17 20:55:24 +0200

    fix: use correct repo

--- .github/scripts/renovate-entrypoint.sh
+++ .github/scripts/renovate-entrypoint.sh
@@ -9,5 +9,11 @@ apt install -y curl bash
 # Install bun
 curl -fsSL https://bun.sh/install | bash
 
+# Make sure bun is loaded
+source /root/.bashrc 
+
+# Print bun version
+bun -v
+
 # Run renovate
-runuser -u ubuntu renovate
+runuser -u ubuntu renovate steveiliop56/runtipi-appstore
